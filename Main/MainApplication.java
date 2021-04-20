169574fdb1b6a6f546c5130160ce0741643bdcc0
Zifang <zifangah@gmail.com>
2021-04-20 14:23:26 -0700

    Print off list

--- Main/MainApplication.java
+++ Main/MainApplication.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class MainApplication {
+public class MainApplication {
+    public static void main(String[] args) {
+        ListNode laa = new ListNode(3);
+        ListNode lab= new ListNode(4, laa);
+        ListNode l1 = new ListNode(2, lab);
+
+        ListNode lba = new ListNode(4);
+        ListNode lbb = new ListNode(6, lba);
+        ListNode l2 = new ListNode(5, lbb);
+        addTwoNumbers(l1, l2);
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        ListNode result = new ListNode();
+        //print off list
+        while(l1!=null && l2!=null) {
+            System.out.println(l1.val + ": " + l2.val);
+            l1=l1.next;
+            l2=l2.next;
+        }
+        //carry over
+
+        return result;
+    }
+
+
+
+
 }
