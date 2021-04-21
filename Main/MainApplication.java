5d8612268cc61dc5a4cce98b0570a3ce0bdd4c20
Zifang <zifangah@gmail.com>
2021-04-20 17:24:09 -0700

    Working solution for given test case, but can be better for consecutive carryover in more test cases

--- Main/MainApplication.java
+++ Main/MainApplication.java
@@ -11,17 +11,43 @@ public class MainApplication {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
-        ListNode result = new ListNode();
+        ListNode result = new ListNode(0);
+        int i, j;
+        int sum = 0;
+        int oldCarry = 0;
+        int newCarry = 0;
         //print off list
         while(l1!=null && l2!=null) {
-            System.out.println(l1.val + ": " + l2.val);
-            l1=l1.next;
-            l2=l2.next;
+            i = l1.val;
+            j = l2.val;
+            sum = (i+j)>=10 ? (i+j)%10 : (i+j);
+            oldCarry = (i+j)>=10 ? 1 : 0;
+            result= new ListNode(sum, result);
+            if(oldCarry>0){
+                System.out.println(l1.val + ": " + l2.val + ": " + result.val);
+                l1=l1.next;
+                l2=l2.next;
+                i = l1.val;
+                j = l2.val;
+                sum = (i+j)>=10 ? (i+j)%10 : (i+j);
+                //newCarry = (i+j)>=10 ? 1 : 0; this needs to be used for another carryover
+                result = new ListNode(sum+oldCarry, result);
+                System.out.println(l1.val + ": " + l2.val + ": " + result.val);
+                l1=l1.next;
+                l2=l2.next;
+            }
+            else{
+                System.out.println(l1.val + ": " + l2.val + ": " + result.val);
+                l1=l1.next;
+                l2=l2.next;
+            }
+
         }
-        //carry over
+        //clean up needed
 
         return result;
     }
+
 
 
 
