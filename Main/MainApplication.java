8c19d759b6a31fd248d8a8bf231058c2961bf4ab
Zifang <zifangah@gmail.com>
2021-04-28 17:34:41 -0700

    Now accounts for multiple carryOver, extra digit

--- Main/MainApplication.java
+++ Main/MainApplication.java
@@ -1,35 +1,57 @@
 public class MainApplication {
     public static void main(String[] args) {
-        ListNode laa = new ListNode(3);
-        ListNode lab= new ListNode(4, laa);
-        ListNode l1 = new ListNode(2, lab);
+        ListNode l1 = new ListNode(2);
+        l1= new ListNode(4, l1);
+        l1 = new ListNode(5, l1);
 
-        ListNode lba = new ListNode(4);
-        ListNode lbb = new ListNode(6, lba);
-        ListNode l2 = new ListNode(5, lbb);
+        ListNode l2 = new ListNode(7);
+        l2 = new ListNode(6, l2);
+        l2 = new ListNode(5, l2);
         addTwoNumbers(l1, l2);
     }
 
-    public static int i, j, sum;
+    public static int i, j, sum, remainder;
     public static int oldCarry = 0;
     public static int curCarry;
+
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
         ListNode result = new ListNode(0);
         while(l1!=null && l2!=null) {
             i = l1.val;
             j = l2.val;
-            sum = (i+j)>=10 ? (i+j)%10 : (i+j);
-            curCarry = (i+j)>=10 ? 1 : 0;
-            result= new ListNode(sum+oldCarry, result);
-            //increment oldCarry after
+            sum = i+j;
+            remainder = (sum+oldCarry)%10;
+            curCarry = (sum+oldCarry) >=10 ? 1 : 0;
             if(curCarry>0){
-                oldCarry++;
-            }
+                //account for multiple carryOver
+                result= new ListNode(remainder, result);
+                oldCarry=1;
+            } else
+                result= new ListNode(sum+oldCarry, result);
             System.out.println(l1.val + ": " + l2.val + ": " + result.val);
+
+            //move up
             l1=l1.next;
             l2=l2.next;
+
+            //carryOver on the last digit
+            if( curCarry>0){
+                    //account for additional digits
+                    if(l1==null && l2!=null){
+                        result= new ListNode(curCarry+l2.val, result);
+                        System.out.println(result.val);
+                    }
+                    if(l2==null && l1!=null){
+                        result= new ListNode(curCarry+l1.val, result);
+                        System.out.println(result.val);
+                    }
+                    //two numbers have same digits
+                    if(l1==null && l2==null){
+                        result= new ListNode(curCarry, result);
+                        System.out.println(result.val);
+                    }
+            }
         }
-        //try a larger list with twice carryover
         return result;
     }
 
